7fd3c4eea107859fb03fcee87bffa40abe770558
ogami-lab <wakamekabudayo@gmail.com>
2025-06-30 23:16:10 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,1 @@
-// javascript.js
-
-function changeVideo(playerId, videoId) {
-  const iframe = document.getElementById(playerId);
-  iframe.src = `https://www.youtube.com/embed/${videoId}`;
-}
-
-// 必要に応じて今後追加機能を拡張できます。
-// 例：自動スライド、トランジション効果、再生制御など
+// JavaScript placeholder
